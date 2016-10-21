6dec5d9767a3a1751d2685c8f5a1080c0bd1095d
firebird31 <abev2@umbc.edu>
2016-10-21 13:32:28 -0400

    broken

--- meeting.php
+++ meeting.php
@@ -8,10 +8,10 @@
 	$advName = $lastName;
 	$meetingPlace = $location;
 	
-	$idQuery = "SELECT `count` FROM advisorData WHERE lName = '".$lastName."'";
-	$rt = $COMMON->executeQuery($idQuery, $_SERVER["SCRIPT_NAME"])
+	//$idQuery = "SELECT `count` FROM advisorData WHERE lName = '".$lastName."'";
+	//$rt = $COMMON->executeQuery($idQuery, $_SERVER["SCRIPT_NAME"])
 	
-	echo $rt
+	//echo $rt
 	
 	//meetingData table does not hold the sessionType but the max number of students
 	//a session can have, so this part translates the 'G'/'I' to a 10/1
@@ -21,6 +21,6 @@
 	else
 		$max_student = 1;
 	$sql = "INSERT INTO meetingData (`count`, `id`, `start_time`, `end_time`, `date`,
-	`max_student`, `num_student`) VALUES (NULL, '"."')";
+	`max_student`, `num_student`) VALUES (NULL, '".$rt."')";
 	$rs = $COMMON->executeQuery($sql, $_SERVER["SCRPIT_NAME"]);
 ?>
